a186625a281e21b203b79ac4438aac6310fbc773
agilbert <agilbert@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-01-27 13:52:47 -0600

    All routing rules can be turned off in app.yml now. Made generator.yml more 1.2 friendly.
    git-svn-id: http://svn.symfony-project.com/plugins/sfShibbolethPlugin/trunk@15000 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@ if (sfConfig::get('app_sf_shibboleth_plugin_routes_register', true) && in_array(
   $this->dispatcher->connect('routing.load_configuration', array('sfShibbolethRouting', 'listenToRoutingLoadConfigurationEvent'));
 }
 
-if (in_array('sfShibbolethUser', sfConfig::get('sf_enabled_modules')))
+if (sfConfig::get('app_sf_shibboleth_plugin_routes_register', true) && in_array('sfShibbolethUser', sfConfig::get('sf_enabled_modules')))
 {
   $this->dispatcher->connect('routing.load_configuration', array('sfShibbolethRouting', 'addRouteForAdminUser'));
 }
