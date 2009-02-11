88293198b8923881670c4e94388e35aa6b76d955
boutell <boutell@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-02-11 09:33:35 -0600

    Fixed redirect loop encountered by Alex
    git-svn-id: http://svn.symfony-project.com/plugins/sfShibbolethPlugin/trunk@15434 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- utilities/subfolder-shim/index.php
+++ utilities/subfolder-shim/index.php
@@ -5,7 +5,19 @@
 # action for us. Note that we're making an assumption
 # that you haven't changed the routing rule. 
 
+# Clean the ever-loving daylights out of the environment so that
+# the front end controller will issue correct redirects
+
+foreach ($_SERVER as $key => &$val)
+{
+  $val = preg_replace("/\/shibboleth(\/index.php|\/|)$/", "/", $val); 
+}
+
+# Run the correct action (note that your routing rules probably
+# vary from ours so you will probably need to change this)
+
 $_SERVER['REQUEST_URI'] = "/cms/sfShibbolethAuth/login";
+$_SERVER['PHP_SELF'] = "/cms/sfShibbolethAuth/login";
 
 require '../index.php';
 
