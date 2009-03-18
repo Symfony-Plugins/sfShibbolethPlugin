175a93f964400de7bbe5ff314ae9a831747874ec
boutell <boutell@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-03-18 15:50:31 -0500

    New logout capabilities
    git-svn-id: http://svn.symfony-project.com/plugins/sfShibbolethPlugin/trunk@16419 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- utilities/subfolder-shim/index.php
+++ utilities/subfolder-shim/index.php
@@ -5,6 +5,23 @@
 # action for us. Note that we're making an assumption
 # that you haven't changed the routing rule. 
 
+# Clear all cookies on logout
+
+if (isset($_GET['logout']))
+{
+  $cookies = array_keys($_COOKIE);
+  foreach ($cookies as $cookie)
+  {
+    // ACHTUNG: only works if we specify the domain ('/' in most cases)
+    setcookie($cookie, "", time() - 3600, '/');
+  }
+  $logout = $_GET['idp-logout'];
+  $returnto = $_GET['returnto'];
+  $logout = str_replace("_RETURNTO_", urlencode($returnto), $logout);
+  header("Location: $logout");
+  exit(0);
+}
+
 # Clean the ever-loving daylights out of the environment so that
 # the front end controller will issue correct redirects
 
